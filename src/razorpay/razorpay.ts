a31a18e6cbc173c289e393b8caad7b8a357ae3f9
Md Mahtab Ansari <m.a.raj58232@gmail.com>
2025-08-28 21:19:15 +0530

    fix: Update PhonePe environment to production and refine Razorpay service code formatting

--- src/razorpay/razorpay.ts
+++ src/razorpay/razorpay.ts
@@ -15,13 +15,13 @@ class RazorpayService {
         this.razorpay = new Razorpay(razorpayConfig);
     }
 
-    async createOrder(amount: number, currency: string, receipt: string,customerDetails?: CustomerDetails) {
+    async createOrder(amount: number, currency: string, receipt: string, customerDetails?: CustomerDetails) {
         try {
             const options = {
                 amount: amount * 100, // Amount in paise
                 currency,
                 receipt,
-                 ...(customerDetails && {
+                ...(customerDetails && {
                     notes: {
                         name: customerDetails.name || '',
                         email: customerDetails.email || '',
@@ -40,7 +40,7 @@ class RazorpayService {
         if (!razorpayConfig.key_secret) {
             throw new Error("Razorpay key secret is not configured");
         }
-        
+
         const generatedSignature = crypto.createHmac("sha256", razorpayConfig.key_secret)
             .update(`${orderId}|${paymentId}`)
             .digest("hex");
